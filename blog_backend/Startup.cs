27481e03b3b7a699acac341673eaa352a5028581
Tung Phan <tungpt.seven@gmail.com>
2019-05-20 10:52:32 +0700

    add missing package

--- blog_backend/Startup.cs
+++ blog_backend/Startup.cs
@@ -23,8 +23,8 @@ namespace blog_backend
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+            
+            // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BlogItemContext>(options =>
